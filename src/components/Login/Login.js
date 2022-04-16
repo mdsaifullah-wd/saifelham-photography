d9ec8536d239f9208227875aa510a70ad790dd30
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-17 03:14:59 +0600

    implement login functionalities

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
+  const inputEmail = useRef('');
+  const inputPassword = useRef('');
+  const navigate = useNavigate();
+  const [signInWithEmailAndPassword, user, loading, errorSignIn] =
+    useSignInWithEmailAndPassword(auth);
+  user && navigate('/');
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    const email = inputEmail.current.value;
+    const password = inputPassword.current.value;
+    // errorCreateUser.message = '';
+    signInWithEmailAndPassword(email, password);
+  };
   return (
     <section className='register-login'>
       <h2 className='form-title'>Please Login</h2>
-      <form className='form-container'>
-        <input type='email' name='email' placeholder='Email' required />
+      <form className='form-container' onSubmit={handleSignIn}>
         <input
+          ref={inputEmail}
+          type='email'
+          name='email'
+          placeholder='Email'
+          required
+        />
+        <input
+          ref={inputPassword}
           type='password'
           name='password'
           placeholder='Password'
           required
         />
+        <p className='error'>
+          {errorSignIn?.message
+            ? errorSignIn.message.includes('auth/wrong-password')
+              ? 'Incorrect password!'
+              : errorSignIn.message.includes('auth/user-not-found')
+              ? 'User not found!'
+              : errorSignIn.message
+            : ''}
+        </p>
         <input type='submit' value='Login' className='btn-form' />
         <Link to={'/register'}>Don't have an account?</Link>
       </form>
