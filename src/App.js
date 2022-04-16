4d35eb603a8ca012bd0b06e0e7e6f179e68f3bb6
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-17 01:09:47 +0600

    add simple inspired section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Banner from './components/Home/Banner/Banner';
+import Inspired from './components/Home/Inspired/Inspired';
 import Services from './components/Home/Services/Services';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header></Header>
       <Banner></Banner>
+      <Inspired></Inspired>
       <Services></Services>
       <Footer></Footer>
     </div>
---
+++ src/components/Home/Inspired/Inspired.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Inspired.css';
+
+const Inspired = () => {
+  return (
+    <section className='inspired'>
+      <div className='container'>
+        <div className='inspired-title'>
+          <div className='divider'></div>
+          <p>Inspired</p>
+          <div className='divider'></div>
+        </div>
+        <h2 className='inspired-heading'>Passion for Excellence</h2>
+        <div className='inspired-text'>
+          <p>
+            When, while the lovely valley teems with vapour around me, and the
+            meridian sun strikes the upper surface of the impenetrable foliage
+            of my trees.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Inspired;
