1c017c80327913e1896867dd96f08b6ed3fbd246
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-17 02:28:34 +0600

    implement firebase sign up basic functionalities

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './Register.css';
 const Register = () => {
+  const inputName = useRef('');
+  const inputEmail = useRef('');
+  const inputPassword = useRef('');
+  const inputConfirmPassword = useRef('');
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+  const [createUserWithEmailAndPassword, user, loading, errorCreateUser] =
+    useCreateUserWithEmailAndPassword(auth);
+  user && navigate('/');
+  errorCreateUser && setError(errorCreateUser);
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    const name = inputName.current.value;
+    const email = inputEmail.current.value;
+    const password = inputPassword.current.value;
+    const confirmPassword = inputConfirmPassword.current.value;
+    if (password !== confirmPassword) {
+      setError("Confirm Password didn't match!");
+      return;
+    }
+    createUserWithEmailAndPassword(email, password);
+  };
   return (
     <section className='register-login'>
       <h2 className='form-title'>Please Register</h2>
-      <form className='form-container'>
-        <input type='text' name='name' placeholder='Name' required />
-        <input type='email' name='email' placeholder='Email' required />
+      <form className='form-container' onSubmit={handleCreateUser}>
         <input
+          ref={inputName}
+          type='text'
+          name='name'
+          placeholder='Name'
+          required
+        />
+        <input
+          ref={inputEmail}
+          type='email'
+          name='email'
+          placeholder='Email'
+          required
+        />
+        <input
+          ref={inputPassword}
           type='password'
           name='password'
           placeholder='Password'
           required
         />
         <input
+          ref={inputConfirmPassword}
           type='password'
           name='confirmPassword'
           placeholder='Confirm Password'
           required
         />
+        <p className='error'>{error}</p>
         <input type='submit' value='Register' className='btn-form' />
         <Link to={'/login'}>Already have an account?</Link>
       </form>
