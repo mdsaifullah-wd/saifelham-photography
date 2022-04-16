b4f648a0f4fafaf3bd207fffec6145037bea30bf
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-17 01:32:29 +0600

    add RequireAuth without functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Checkout from './components/Checkout/Checkout';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -11,7 +12,13 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/checkout' element={<Checkout></Checkout>}></Route>
+        <Route
+          path='/checkout'
+          element={
+            <RequireAuth>
+              <Checkout></Checkout>
+            </RequireAuth>
+          }></Route>
       </Routes>
       <Footer></Footer>
     </div>
