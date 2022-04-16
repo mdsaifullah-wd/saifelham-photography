5c8d0106d58a8d8fab6d4a1f45600eeecfd5a7f9
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-17 00:55:05 +0600

    add simple footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Banner from './components/Home/Banner/Banner';
 import Services from './components/Home/Services/Services';
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Banner></Banner>
       <Services></Services>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='container'>
+        <div className='footer-container'>
+          <p>Copyright &copy; 2022. All right reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
