547e4a3f205de068091e82a2d6e8fbc2b8f0f7de
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-17 03:26:32 +0600

    make checkout route protected

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const RequireAuth = ({ children }) => {
-  return <>{children}</>;
+  const location = useLocation();
+  const [user] = useAuthState(auth);
+  if (!user) {
+    return <Navigate to='/login' state={{ from: location }} replace />;
+  }
+  return children;
 };
 
 export default RequireAuth;
