10860d214afc180e2c792231157e9d80f2dd10c9
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-18 23:53:59 +0600

    update checkout page

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 import './Checkout.css';
 
 const Checkout = () => {
+  const handleCheckout = (event) => {
+    event.preventDefault();
+    toast('Thanks for purchasing my Service!');
+  };
   return (
     <section className='checkout'>
-      <div className='container'>
-        <h2 className='text-center'>Checkout Page</h2>
-      </div>
+      <h2 className='section-title'>Provide Information</h2>
+      <form className='form-container' onSubmit={handleCheckout}>
+        <input type='text' name='name' placeholder='Name' required />
+        <input type='email' name='email' placeholder='Email' required />
+        <input type='text' name='address' placeholder='Address' required />
+        <input
+          type='number'
+          name='number'
+          placeholder='Phone Number'
+          required
+        />
+
+        <input
+          type='submit'
+          value='Confirm Purchase'
+          className='btn btn-primary'
+        />
+      </form>
+      <ToastContainer />
     </section>
   );
 };
