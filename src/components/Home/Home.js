679218bf28bd65151734b1cb932b29424dbd8029
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-18 19:04:49 +0600

    update inspiration section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import './Home.css';
-import Inspired from './Inspired/Inspired';
+import Inspiration from './Inspiration/Inspiration';
 import Services from './Services/Services';
 
 const Home = () => {
   return (
     <>
       <Banner></Banner>
-      <Inspired></Inspired>
+      <Inspiration></Inspiration>
       <Services></Services>
     </>
   );
---
+++ src/components/Home/Inspiration/Inspiration.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Inspiration.css';
+
+const Inspiration = () => {
+  return (
+    <section className='inspiration'>
+      <div className='container'>
+        <div className='inspiration-title'>
+          <div className='divider'></div>
+          <p>Inspiration</p>
+          <div className='divider'></div>
+        </div>
+        <h2 className='inspiration-heading'>Photography is an Art</h2>
+        <div className='inspiration-text'>
+          <p>
+            When words become unclear, I shall focus with photographs. When
+            images become inadequate, I shall be content with silence.
+            Essentially what photography is is life lit up.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Inspiration;
