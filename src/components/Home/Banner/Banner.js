3227c6c512b99181cc4c54155444b264756cffc6
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-18 18:56:12 +0600

    update banner

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -9,10 +9,10 @@ const Banner = () => {
           <div className='banner-text'>
             <div>
               <h2>Hi, I am a Photographer</h2>
-              <h2>
-                I capture <span>Moments</span>
-              </h2>
-              <button className='btn'>Hire Me</button>
+              <h3>
+                I capture <span className='bold'>Moments</span>
+              </h3>
+              <button className='btn btn-transparent btn-large'>Hire Me</button>
             </div>
           </div>
         </div>
